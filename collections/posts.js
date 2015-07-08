a2879ff0d9ab11dddf6c0160ff022df1400061c0
hexes <trapp2357@gmail.com>
2015-07-08 12:49:39 +0000

    added create posts with security

--- collections/posts.js
+++ collections/posts.js
@@ -1,1 +1,29 @@
 Posts = new Mongo.Collection('posts');
+
+Meteor.methods({
+    postInsert: function(postAttributes) {
+        check(Meteor.userId(), String);
+        check(postAttributes, {
+            title: string,
+            url: string
+        });
+        
+        var postWithSameLink = Posts.findOne({url :postAttributes.url});
+        if (postWithSameLink) {
+            return {
+                postExists_id: postWithSameLink._id
+            }
+        }
+                
+        var user = Meteor.user();
+        var post = _.extend(postAttributes, {
+            userId: user._id,
+            author: user.username,
+            submitted: new Date()
+        });
+        var postId = Posts.insert(post);
+        return {
+            _id: postId
+        };
+    }
+});
